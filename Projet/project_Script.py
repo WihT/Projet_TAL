8289af9fd849bbeca8d6567a45732bcf989f4c77
WihT <th.witczak@gmail.com>
2018-03-26 16:04:59 +0200

    Add files via upload

--- Projet/project_Script.py
+++ Projet/project_Script.py
@@ -3,8 +3,30 @@ import random
 import re
 import argparse
 
-def bail(arg) :
-	#TODO
-	return
+prevchoice = 0
 
-bail("hello")
+def answers(x):
+    return {
+        1 : "hmm",
+        2 : "uh huh",
+        3 : "continue",
+        4 : "yes",
+    }[x]
+
+def bot(answer) :
+	global prevchoice
+	choice = random.randint(1,4)
+	while prevchoice == choice :
+		choice = random.randint(1,4)
+	print("Bob : "+answers(choice))
+	prevchoice = choice
+	if (answer == "Bye") or (answer == "bye") :
+		print("Bob : See you!")
+		return False
+	return True
+
+print("Bob : Hello")
+while True:
+    if bot(input("You : ")) == False :
+        break
+    
