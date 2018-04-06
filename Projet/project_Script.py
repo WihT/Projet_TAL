545222f7fabe9dec47c7026a7f5c55e2949a0f59
WihT <th.witczak@gmail.com>
2018-04-06 12:13:34 +0200

    road to mode2

--- Projet/project_Script.py
+++ Projet/project_Script.py
@@ -5,7 +5,7 @@ import argparse
 
 prevChoice = 0
 
-def ansMode1():
+def ansMode1() :
 	global prevChoice
 	x = random.randint(0,1024)%6
 	while prevChoice == x :
@@ -20,14 +20,31 @@ def ansMode1():
         5 : "Yes...",
     }[x]
 
-def bot(answer) :
+def ansMode2(answer) :
+	ansWords = re.split("[ .,?!/()]+", answer)
+	print(ansWords)
+	return
+
+def bot(answer) : 
 	if (answer == "Bye") or (answer == "bye") :
 		print("Bob : See you !")
 		return False
-	print("Bob : "+ansMode1())
+	#print("Bob : "+ansMode1())
+	ansMode2(answer)
 	return True
 
 #main
+with open("lexicals.txt","r") as filepointer :
+	# lecture du fichier de champs lexicaux
+	content = filepointer.read()
+	tmp = re.split("\n\n\t+", content)
+	lexicals = []
+	for lex in tmp :
+		lexicals.append(re.split("\n+", lex))
+
+del lexicals[len(lexicals)-1][len(lexicals[len(lexicals)-1])-1]
+print(lexicals)
+
 print("Bob : Hello")
 while True:
     if bot(input("You : ")) == False :
