2b3983f136dce260732773f8386243723fcd738f
FloVollmer <vollmerflorent@gmail.com>
2018-04-22 14:18:13 +0200

    currsubj is in lexFields. Adjustments in mode2

--- Projet/lexicalField.py
+++ Projet/lexicalField.py
@@ -2,16 +2,27 @@ import re
 
 class LexField:
 	"""Representation of a lexical field"""
-	keyWords = []
-	keyGroups = []
-	answers = []
-	parentFieldsID = []
 	
 	def __init__(self, content):
 		self.paragraphs = content.split("\n\n")
 		self.keyWords = self.paragraphs[0].split("\n")
 		self.answers = self.paragraphs[1].split("\n")
+		self.parents = []
+		self.pertinent = 0
 		#print(str(self))
 		
 	def __str__(self):
 		return str(self.keyWords) + "\n" + str(self.answers)
+		
+	def __repr__(self):
+		return str('{0:.2f}'.format(self.pertinent))
+		
+	def decrement(self):
+		self.pertinent *= 0.7
+		if self.pertinent < 0.5:
+			self.pertinent = 0
+			
+	def increment(self, value):
+		self.pertinent += value
+		for subj in self.parents:
+			subj.increment(value/2)
--- Projet/main.py
+++ Projet/main.py
@@ -7,7 +7,7 @@ import numpy as np
 from lexicalField import LexField
     
 def ansMode1(prevChoice) :
-	choice = random.randint(0, 3)
+	choice = random.randint(0, 4)
 	if choice >= prevChoice :
 		choice += 1
 	str = {
@@ -16,38 +16,42 @@ def ansMode1(prevChoice) :
         2 : "Continue.",
 		3 : "Tell me more.",
         4 : "I see...",
+		5 : "Oh really?"
     }[choice]
 	print ("Bob : " + str)
 	return choice
 
 
-def ansMode2(answer, lexFields, currSubj, prevChoice) :
+def ansMode2(answer, lexFields, prevChoice) :
 	for wAns in answer :
 		for iLex in range(len(lexFields)) :
 			for wLex in lexFields[iLex].keyWords :
 				if matchLex(wAns, wLex) :
 					#print("I recognized the word " + wLex + " from lexical \"" + lexicals[iLex][0] + "\"")
-					currSubj[iLex] += 1
-    
-	maxSubj = currSubj.index(np.amax(currSubj))
-	#print(lexFields[0].answers)
-	choice = random.randint(0, len(lexFields[maxSubj].answers)-2)
-	if choice >= prevChoice :
-		choice += 1
-	prevChoice = choice
-
-	mode2 = False
-
-	for i in range(0, len(currSubj)) :
-		if currSubj[i] != 0 :
-			mode2 = True
-			break
-        
-	if mode2 == False :
-		return -1
-	else :
+					lexFields[iLex].increment(1)
+					
+	maxSubjs = []
+	maxPertinence = 0
+	for iSubj in range(len(lexFields)):
+		if lexFields[iSubj].pertinent > maxPertinence:
+			maxPertinence = lexFields[iSubj].pertinent
+			maxSubjs = []
+			maxSubjs.append(iSubj)
+		elif lexFields[iSubj].pertinent == maxPertinence and maxPertinence > 0:
+			maxSubjs.append(iSubj)
+	
+	# if there are several equally pertinent subjects, Bob chooses one randomly
+	if (len(maxSubjs) > 0):
+		maxSubj = maxSubjs[random.randint(0, len(maxSubjs)-1)]
+		choice = random.randint(0, len(lexFields[maxSubj].answers)-2)
+		if choice >= prevChoice :
+			choice += 1
+		prevChoice = choice
 		print ("Bob : " + lexFields[maxSubj].answers[choice])
 		return choice
+	else:
+		return -1
+		
 
 def checkYes(answer) :
     for word in answer :
@@ -68,7 +72,7 @@ def checkNo(answer) :
                 return True
             else : 
                 return False
-def ansMode3(answer, lexFields, currSubj, prevChoice, sympathy) :
+def ansMode3(answer, lexFields, prevChoice, sympathy) :
    if checkYes(answer) :
        print("*Bob writes it in his notebook.*")
        sympathy += 1
@@ -90,18 +94,18 @@ def matchLex(wAns, wLex) :
 	return wAns == wLex
 
 	
-def bot(answer, lexFields, currSubj, prevChoice, sympathy) :
+def bot(answer, lexFields, prevChoice, sympathy) :
 	if (answer == "Bye") or (answer == "bye") :
 		print("Bob : See you !")
 		return -1
 	
 	ansWords = re.split("[ .,?!/()]+", answer)
     
-	choice, sympathy = ansMode3(ansWords, lexFields, currSubj, prevChoice, sympathy)
+	choice, sympathy = ansMode3(ansWords, lexFields, prevChoice, sympathy)
 	if sympathy <= -5 :
 		return choice, sympathy
 	if  choice == -1 :
-		choice = ansMode2(ansWords, lexFields, currSubj, prevChoice)
+		choice = ansMode2(ansWords, lexFields, prevChoice)
 		if choice == -1 :
 			choice = ansMode1(prevChoice)
 	return choice, sympathy
@@ -112,28 +116,26 @@ def bot(answer, lexFields, currSubj, prevChoice, sympathy) :
 with open("lexFields.txt","r") as filepointer :
 	# lecture du fichier de champs lexicaux
 	content = filepointer.read()
-	tmp = re.split("\n\n\t+", content)
+	tmp = re.split("\n\n\t", content)
 	lexFields = []
 	for lex in tmp :
 		lexFields.append(LexField(lex))
 		
-currSubj = [0] * len(lexFields)
 sympathy = 0
 prevChoice = 0
 
 print("Bob : Hello")
 print("Bob : Please tell me something about you.")
 while True:
-	choice, sympathy = bot(input("You : "), lexFields, currSubj, prevChoice, sympathy)
+	choice, sympathy = bot(input("You : "), lexFields, prevChoice, sympathy)
 	if sympathy <= -5 :
 		print("You monster !")
 		print("*Bob run away from you*")
 		break
 	if  choice == -1 :
 		break
-	print(currSubj)
-	for iSubj in range(len(currSubj)) :
-		currSubj[iSubj] /= 2;
-		if (currSubj[iSubj] < 1) :
-			currSubj[iSubj] = 0;
+	#print(lexFields)
+	for subj in lexFields :
+		subj.decrement()
 	prevChoice = choice
+	#print(lexFields)
